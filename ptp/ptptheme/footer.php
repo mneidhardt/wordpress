9a8a7835cff2838c0efa047a127623a7b5ec8b16
Michael Neidhardt <mneidhardt@gmail.com>
2015-08-01 12:47:35 +0200

    Added ptp, i.e. ptptheme and ptpplugin.

---
+++ ptp/ptptheme/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage ptptheme
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+            ------------me@example.org------------
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ ptp/ptptheme/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage ptptheme
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="stylesheet" type="text/css" href="/style.css">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="headerbox">
+<a href="/">Works</a> | <a href="/10-2/">Info</a> | Contact
+</div>
+<div style="clear: both;"></div>
+<div id="site">
+  <div id="site">
+
+  <div id="content" class="site-content">
