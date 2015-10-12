69f3af29b23737deb9ee6a93a94dd3ea59c7d0bc
Michael Neidhardt <mneidhardt@gmail.com>
2015-10-12 21:56:14 +0200

    Now Settings and Bulk import are two separate forms, on same page.

--- ptp/ptptheme/ptpsettings.php
+++ ptp/ptptheme/ptpsettings.php
@@ -17,23 +17,18 @@ function ptpconfig_menufunc() {
     }
 
     $option1 = 'ptppicsperrow';
-    $option2 = 'ptpbulkimageimport';
-    $hidden = 'ptpsettingshidden';
+    $option2 = 'ptpbulkimportsubdir';
+    $option3 = 'ptpbulkimportcategory';
+    $hidden1 = 'hidden1';
+    $hidden2 = 'hidden2';
 
     $picsperrow = get_option($option1);
 
     // See if the user has posted us some information
     // If they did, this hidden field will be set to 'Y'
-    if( isset($_POST[ $hidden ]) && $_POST[ $hidden ] == 'Y' ) {
+    if( isset($_POST[ $hidden1 ]) && $_POST[ $hidden1 ] == 'Y' ) {
         // Read their posted value
         $picsperrow = $_POST[ $option1 ];
-        $bulkimport = $_POST[ $option2 ];
-
-        if (isset($bulkimport) && $bulkimport == 'on') {
-            bulkimport();
-        }
-
-        // Save the posted value in the database
         if ( ! preg_match('/^\d{1,3}$/', $picsperrow)) {
             $picsperrow = 3;
         }
@@ -41,33 +36,35 @@ function ptpconfig_menufunc() {
 
         // Put a "settings saved" message on the screen
         print('<div class="updated"><p><strong>New values saved.</strong></p></div>');
+    } elseif( isset($_POST[ $hidden2 ]) && $_POST[ $hidden2 ] == 'Y' ) {
+        $result = bulkimport($_POST[$option2], $_POST[$option3]);
+        print('<div class="updated"><p><strong>Attempted image import: ' . $result . '</strong></p></div>');
     }
 
     print('<div class="wrap"><h2>' . __('Setup stuff', 'menu-test') . '</h2><table class="form-table">');
     print('<form name="form1" method="post" action="">');
-    print('<input type="hidden" name="' . $hidden . '" value="Y">');
+    print('<input type="hidden" name="' . $hidden1 . '" value="Y">');
     print('<tr><th scope="row"><label for="' . $option1 . '">Pictures per row on main page:</label></th>');
     print('<td><input type="number" name="' . $option1 . '" value="' . $picsperrow . '" size="45" /></td></tr>');
-    print('<tr><th scope="row"><label for="' . $option2 . '">Bulk import images?</label></th>');
-    print('<td><input type="checkbox" name="' . $option2 . '" /></td></tr>');
     print('<tr><td colspan="2"><input type="submit" name="Submit" value="Save" class="button-primary"/></td></tr>');
+    print('</form></table><hr>');
+
+    print('Bulk image import<br><table class="form-table">');
+    print('<form name="form2" method="post" action="">');
+    print('<input type="hidden" name="' . $hidden2 . '" value="Y">');
+    print('<tr><th scope="row"><label for="' . $option2 . '">Location<br>(below uploads dir):</label></th>');
+    print('<td><input type="text" name="' . $option2 . '" size="45" /></td></tr>');
+    print('<tr><th scope="row"><label for="' . $option3 . '">Category</label></th>');
+    print('<td><input type="text" name="' . $option3 . '" /></td></tr>');
+    print('<tr><td colspan="2"><input type="submit" name="Import" value="Import" class="button-primary"/></td></tr>');
     print('</form></table></div>');
 }
 
-function bulkimport() {
+function bulkimport($subdir, $category) {
     // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
     require_once( ABSPATH . 'wp-admin/includes/image.php' );
-    // $dir = wp_upload_dir()['basedir'] . '/bulk';
     $dir = wp_upload_dir();
-
-return;
-// Not doing anything here unless u remove the return stmt...
-
-
-    $imagesubdir = 'tokyo2';
-    $category4images = 'Tokyo';
-
-    $files = glob($dir['basedir'] . "/$imagesubdir/*");
+    $files = glob($dir['basedir'] . "/bulk/$subdir/*");
 
     if (is_array($files) && sizeof($files) > 0) {
         asort($files);
@@ -77,10 +74,10 @@ return;
             ++$count;
     
            $post = array(
-               'post_title' => $category4images,
+               'post_title' => $category,
                'post_content' => '',
                'post_status' => 'publish',
-               'post_category'  => array(get_cat_id($category4images))
+               'post_category'  => array(get_cat_id($category))
                );
     
            $PID = wp_insert_post($post);
@@ -102,7 +99,9 @@ return;
            set_post_thumbnail($PID, $attach_id);
         }
     
-        // error_log(print_r($files, true));
+        return('Imported ' . $count . ' files. Found ' . sizeof($files) . ' in ' . $subdir . ' Catg=' . $category);
+    } else {
+        return('Did not import anything.');
     }
 }
 /* ----------------------------------------------------------------------------------------------------------- */
